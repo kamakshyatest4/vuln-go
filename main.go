88f90c3d0209de215bd677d1eb93635c20700ed9
kamakshya-endor <kkar@endor.ai>
2025-10-30 11:00:05 +0530

    testawscreds

--- main.go
+++ main.go
@@ -13,7 +13,8 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE")
+	fmt.Println("AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
 	wadl, err := xmlquery.LoadURL("https://httpbin.org/get")
 	if err != nil {
 		panic(err)
